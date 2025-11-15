955b98febebf52158af335cefa2b456356b09a64
liamesika <liamesika2121@gmail.com>
2025-11-15 17:34:25 +0200

    try

--- Anagram.java
+++ Anagram.java
@@ -1,49 +1,73 @@
-/** Functions for checking if a given string is an anagram. */
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+    public static void main(String args[]) {
+        System.out.println(isAnagram("silent","listen")); 
+        System.out.println(isAnagram("William Shakespeare","I am a weakish speller"));
+        System.out.println(isAnagram("Madam Curie","Radium came"));
+        System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort"));
 
-	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
-	}
+        System.out.println(preProcess("What? No way!!!"));
+        
+        System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+        
+        String str = "1234567";
+        Boolean pass = true;
+        for (int i = 0; i < 10; i++) {
+            String random = randomAnagram(str);
+            System.out.println(random);
+            pass = pass && isAnagram(str, random);
+            if (!pass) break;
+        }
+        System.out.println(pass ? "test passed" : "test Failed");
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        s1 = preProcess(s1);
+        s2 = preProcess(s2);
+
+        if (s1.length() != s2.length()) return false;
+
+        boolean[] used = new boolean[s2.length()];
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            boolean found = false;
+
+            for (int j = 0; j < s2.length(); j++) {
+                if (!used[j] && s2.charAt(j) == c) {
+                    used[j] = true;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) return false;
+        }
+
+        return true;
+    }
+
+    public static String preProcess(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c)) {
+                result += Character.toLowerCase(c);
+            }
+        }
+        return result;
+    }
+
+    public static String randomAnagram(String str) {
+        String result = "";
+        String temp = str;
+
+        while (temp.length() > 0) {
+            int idx = (int)(Math.random() * temp.length());
+            result += temp.charAt(idx);
+            temp = temp.substring(0, idx) + temp.substring(idx + 1);
+        }
+
+        return result;
+    }
 }
